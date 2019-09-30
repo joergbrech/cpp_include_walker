770ec53f95982daecb49ecbb1834b509a5386555
Jan Kleinert <jan.kleinert@dlr.de>
2019-09-30 13:27:26 +0200

    some functional programming

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # C++ Include Walker
 
-Its probably really stupid to write a C++ dependency walker with rust, but hey!
-
 The idea is to parse all of the `#include` statements in a set of `.cpp`, `.cxx`, `.c`, `.h`, `.hpp`, `.hxx` files, build a *depencency forest* and provide some functionality, such as
  - finding circular dependencies, 
- - finding superfluous `#include` statements
+ - finding superfluous `#include` statements *(superfluous in the sense of "include only what you need)*
  - sort header files topologically.
  
-In the long run, it would be awesome if we could automatically generate `.i` files for swig using this. Or maybe create automated PRs to clean up C++ projects on Github/Gitlab?
+In the long run, it would be awesome if we could use this to automatically generate `.i` files for swig using this. 
+
+## Note
+
+Other tools use a compiler to print aut the AST.
+
+ - **Con**:
+ Additional dependency, how would you do this platform-independently? 
+ - **Pro**: This would be useful to catch platform dependent includes and not make assumptions about include guards.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::fs::File;
+use std::fs;
+use std::fs::{File, metadata};
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::vec::Vec;
+use std::collections::HashMap;
 
 extern crate nom;
 use nom::sequence::delimited;
@@ -9,7 +11,7 @@ use nom::character::complete::{char, multispace0};
 use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag};
 
-// get all dependencies of a cpp file
+// parse all dependencies of a cpp source or header file
 fn get_deps<P>(filename: P) -> Vec::<String>
 where P: AsRef<Path>, {
     let mut deps = Vec::<String>::new();
@@ -34,7 +36,7 @@ where P: AsRef<Path>, {
     return deps;
 }
 
-// helper function for file io
+// helper function for file parsing
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -56,10 +58,69 @@ fn include_parser(input: &str) -> nom::IResult<&str, &str> {
   Ok((input, header))
 }
 
+// recursively find all files and apply function func to the path of the file
+fn ls_src<P,F>(dir: P, func: &mut F, recursive: bool)
+where P: AsRef<Path> + Copy, 
+      F: FnMut(String)
+{
+    match fs::read_dir(dir) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(paths) => for path in paths {
+            let p = path.unwrap().path();
+            let md = metadata(&p).unwrap();
+
+            if md.is_dir() {
+                if recursive {
+                    // recurse into subdir
+                    ls_src(p.to_str().unwrap(), func, recursive);
+                }
+            }
+            else {
+                // apply func to path of file
+                func(p.to_str().unwrap().to_string());
+            }
+        },
+    }    
+}
+
+/*
+struct dependency_node {
+    key: String,                     // a uid of the dependency
+    path: Option<String>,     // path to file, if it exists in the searched directory
+    used_by: Vec<String>,            // adjacency list (who depends on me)
+}
+
+struct dependency_tree {
+    node_map: HashMap<String, dependency_node>,
+}
+
+impl dependency_tree {
+    fn fill_from_directory<P>(&self, dir: P) {
+            //TODO
+    }
+}
+*/
+
 fn main() {
+    // parse all #include statements of one file
     let filename = "/home/jan/winhome/Tools/tigl/src/engine_nacelle/CCPACSNacelleCenterCowl.cpp";
+    println!("{:?}", get_deps(filename));
 
-    let deps = get_deps(filename);
-    println!("{:?}", deps);
+    // recursive ls *.h, *.hpp, *.hxx, *.c, *.cpp, *.cxx
+    let src_dir = "/home/jan/winhome/Tools/tigl/src";
 
+    let mut res = Vec::<String>::new();
+    ls_src(src_dir, &mut |p| res.push(p), false);
+
+    // strip the src_dir from the found files
+    for idx in 0..res.len() {
+        let stripped_path_res = Path::new(&res[idx]).strip_prefix(src_dir);
+        match stripped_path_res {
+            Err(why) => println!("! {:?}", why), 
+            Ok(stripped_path) => res[idx] = stripped_path.to_str().unwrap().to_string(),
+        }
+    }
+    println!("{:?}", res);
+    println!("{:?}", res.len())
+    
 }
