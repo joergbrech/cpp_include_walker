7062b95dd6bd680166fdfc05f92e5c17c7c3f2b2
Jan Kleinert <jan.kleinert@dlr.de>
2019-10-01 11:02:31 +0200

    remove graph example for now

--- src/simple_graph.rs
+++ src/simple_graph.rs
@@ -1,45 +1,5 @@
 /// Simple graph functionality
-///
-/// # Example
-///
-/// ```
-/// // There is a special order to getting dressed in the morning
-/// use std::collections::HashMap;
-/// let mut before = HashMap::new();
-/// before.insert("jacket",  vec!["shirt"]);
-/// before.insert("whities", vec![]);
-/// before.insert("shirt",   vec![]);
-/// before.insert("socks",   vec![]);
-/// before.insert("hat",     vec!["shirt"]);
-/// before.insert("pants",   vec!["socks", "whities"]);
-/// /*
-/// // j in adj[i] <=> we need to put on nodes[i] before we put on nodes[j]
-/// let adj = vec![ vec![2], vec![], vec![], vec![], vec![2], vec![2], vec![1, 3]];
-///
-/// // Now lets make is a graph
-/// struct Clothes(Vec::<&'static str>, Vec::<Vec::<i32>>);
-///
-/// impl SimpleGraph for Clothes {
-///
-///    type N = &'static str;
-///
-///    fn children(&self, node: &Self::N) -> Vec::<&Self::N> {
-///        let mut v = Vec::<&Self::N>::new();
-///        v.reserve(node.uses.len());
-///        for i in 0..node.uses.len() {
-///            v.push(&self.node_map[&node.uses[i]]);
-///        } 
-///        v
-///    }
-///
-///    fn get_nodes(&self) -> Vec::<&Self::N> {
-///        self[0]
-///    }
-/// }
-/// 
-/// let clothes = Clothes(nodes, adj);
-/// */
-/// ````
+
 
 /// A trait for simple graphs
 pub trait SimpleGraph {
