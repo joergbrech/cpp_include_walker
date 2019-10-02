d271e5c491650dad97e131310b6b097cace9a24e
Jan Kleinert <jan.kleinert@dlr.de>
2019-10-02 23:47:40 +0200

    implement topo_sort

--- src/simple_graph.rs
+++ src/simple_graph.rs
@@ -1,32 +1,48 @@
-//! simple graph functionality specially purposed for topo sort
+//! simple graph functionality specially purposed for topological sorting
 
-/// A trait for simple graphs
+/// A simple graph implementation on unordered borrowed data
 pub trait SimpleGraph {
 
     /// Node type
-    type N;
+    type N : PartialEq + std::fmt::Debug;
 
     /// get all nodes
-    fn get_nodes(&self) -> Vec::<&Self::N>;
+    fn nodes(&self) -> Vec::<&Self::N>;
 
     /// get all children of a node
     fn children(&self, node: &Self::N) -> Vec::<&Self::N>;
 
-    /// returns the number of nodes
-    fn len(&self) -> usize {
-        self.get_nodes().len()
+    /// get all ancestors of a node
+    /// 
+    /// This function is slow without any additional assumptions on the generic type and memory management. 
+    /// Usually, this function can be implemented more efficiently and it is recommended
+    /// to explicitly implement it.
+    fn ancestors(&self, node: &Self::N) -> Vec::<&Self::N> {
+        let mut res = Vec::<&Self::N>::new();
+        let nodes = self.nodes();
+        for other in nodes {
+            let children = self.children(other);
+            for child in children {
+                if child == node {
+                    res.push(other);
+                }
+            }
+        }
+        return res;
     }
 
     /// get topologically sorted vector of nodes for a graph
     fn get_topological_order(&self) -> Result<Vec::<&Self::N>, &'static str> {
 
         // Kahn's method
-        let nodes = self.get_nodes();
+        let nodes = self.nodes();
         let n = nodes.len();
 
         // calculate in-degree
-        let mut in_degree = vec!(0; n);
-        // TODO!!
+        let mut in_degree = vec![0; n];
+        for i in 0..n {
+            in_degree[i] = self.ancestors(nodes[i]).len();
+        }
 
         // initialize candidate list and return list
         let mut candidates = Vec::<&Self::N>::new();
@@ -44,10 +60,12 @@ pub trait SimpleGraph {
             list.push(node);
 
             let children = self.children(node);
-            for i in 0..children.len() {
-                in_degree[i] -= 1;
-                if in_degree[i] == 0 {
-                    candidates.push(nodes[i]);
+            for child in children {
+                //TODO is there a faster way to get the index of the child?
+                let idx = nodes.iter().position(|&x| x==child).unwrap();
+                in_degree[idx] -= 1;
+                if in_degree[idx] == 0 {
+                    candidates.push(nodes[idx]);
                 }
             }
         }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,11 +4,12 @@ use cpp_include_walker::simple_graph::SimpleGraph;
 #[test]
 fn integration_test() {
     let mut forest: DependencyForest = Default::default();
-    forest.fill_from_directory("tests/test_data/circular_dep", true);
-    let topo_sort = forest.get_topological_order().unwrap();
+    forest.fill_from_directory("tests/test_data/simple", true);
+    let topo_sort = forest.get_include_order().unwrap();
 
     assert_eq!(forest.len(), 4);
 
+    println!("{:?}", topo_sort);
     assert_eq!(topo_sort[0].uses.len(), 0);
     assert_eq!(topo_sort[0].used_by, ["a_hdr", "b_hdr"]);
 
