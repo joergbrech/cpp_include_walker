99058ad37fb0458f16b048a3c9d14ced7d516832
Jan Kleinert <jan.kleinert@dlr.de>
2019-10-03 21:25:35 +0200

    add another benchmark

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -6,7 +6,13 @@ use test::Bencher;
 extern crate test;
 
 #[bench]
-fn bench_tigl_include_order(b: &mut Bencher) {
+fn tigl_parse_includes(b: &mut Bencher) {
+    let mut forest: DependencyForest = Default::default();
+    b.iter(|| forest.fill_from_directory("../tigl/src/", true));
+}
+
+#[bench]
+fn tigl_include_order(b: &mut Bencher) {
     let mut forest: DependencyForest = Default::default();
     forest.fill_from_directory("../tigl/src/", true);
     b.iter(|| forest.include_order(true));
